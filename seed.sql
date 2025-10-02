8cfaa29a21c8de9508efff7648384cd8281104e1
Nikhil Inja <realninjadev@gmail.com>
2025-10-01 22:20:05 -0500

    seed sql

---
+++ seed.sql
@@ -0,0 +1,6 @@
+\copy employees FROM 'C:/Users/nikhi/OneDrive/Documents/331-Project-2/db_population_csvs/employees.csv' DELIMITER ',' CSV HEADER;
+\copy inventory FROM 'C:/Users/nikhi/OneDrive/Documents/331-Project-2/db_population_csvs/inventory.csv' DELIMITER ',' CSV HEADER;
+\copy menu FROM 'C:/Users/nikhi/OneDrive/Documents/331-Project-2/db_population_csvs/menu.csv' DELIMITER ',' CSV HEADER;
+\copy customerRewards FROM 'C:/Users/nikhi/OneDrive/Documents/331-Project-2/db_population_csvs/customerRewards.csv' DELIMITER ',' CSV HEADER;
+\copy transactions FROM 'C:/Users/nikhi/OneDrive/Documents/331-Project-2/db_population_csvs/transactions.csv' DELIMITER ',' CSV HEADER;
+\copy sales FROM 'C:/Users/nikhi/OneDrive/Documents/331-Project-2/db_population_csvs/sales.csv' DELIMITER ',' CSV HEADER;
